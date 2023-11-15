ff4dbc172507e71fd7808354ec26972a8a0df45b
marybogyo <bratis.marianna@gmail.com>
2023-11-15 19:43:04 +0100

    manualis adat

--- repulo/database/migrations/2014_10_12_000000_create_users_table.php
+++ repulo/database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,8 @@ return new class extends Migration
             $table->rememberToken();
             $table->timestamps();
         });
+
+        
     }
 
     /**
--- repulo/database/migrations/2023_11_15_165630_create_airlines_table.php
+++ repulo/database/migrations/2023_11_15_165630_create_airlines_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Airline;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -17,6 +18,12 @@ return new class extends Migration
             $table->string('country');
             $table->timestamps();
         });
+
+        Airline::create(
+            [
+                'name' => 'Gizi', 'country'=>'Dánia'
+            ]
+            );
     }
 
     /**
